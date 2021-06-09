50472628b2b8c4360556dad4b589deff0864818a
Theo Laurent <ze_king_to@hotmail.com>
2021-06-09 13:52:18 +0200

    re

--- JS/panier.js
+++ JS/panier.js
@@ -83,7 +83,7 @@ function formulaireContactValid() {
           localStorage.setItem("paiement", totalComplet);
           localStorage.setItem("nbrArticles", nbrArticles);
           // on redirige l'utilisateur vers la page confirmation.html lors de la fin de la fonction
-          window.location.href = "https://theo-laurent.github.io/P5-Front-Orinoco/confirmation.html";
+          window.location.href = "./confirmation.html";
         });
     } else {
       alert("Veuillez remplir le formulaire correctement");
